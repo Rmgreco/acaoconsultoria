5da9cd3a046bed4dc8c7d73cb6282cce0cab6188
Rmgreco <rafaelmgreco@hotmail.com>
2023-12-25 21:03:29 -0300

    make it responsive

--- front-end/components/userCard/UserCard.Styles.js
+++ front-end/components/userCard/UserCard.Styles.js
@@ -3,13 +3,16 @@ import styled from "styled-components";
 export const CardContainer = styled.div`
   border: 1px solid #ddd;
   display: flex;
-  /* flex-direction: ${({ vertical }) => (vertical=="true" ? 'column' : 'row')}; */
   padding: 10px;
   margin: 10px;
   align-items: center;
   width:100%;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   border-radius: 8px; 
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
 `;
 
 export const InfoContainer = styled.div`
@@ -19,8 +22,12 @@ export const InfoContainer = styled.div`
   padding-left: 50px;
   padding-right: 20px;
   width: 50%;
-  overflow:hidden;
   flex-wrap:nowrap;
+
+  @media (max-width: 800px) {
+    width: 100%;
+border-right:none;
+  }
 `;
 
 // export const ButtonContainer = styled.div`
